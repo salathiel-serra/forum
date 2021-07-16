ee01a9a03c665900a1e2f96f27f2742797f17602
Thiel Serra <salathiel.serra@stc.ma.gov.br>
2021-07-16 00:54:56 -0300

    Filtrando a listagem de modulos e recursos de acordo com o usuário logado

--- app/Http/Views/MenuViewComposer.php
+++ app/Http/Views/MenuViewComposer.php
@@ -4,10 +4,20 @@ namespace App\Http\Views;
 class MenuViewComposer {
   public function compose($view)
   {
-    $menus = auth()->user()
-                    ->role
-                    ->modules;
-                    
-    return $view->with('menus', $menus);
+    $roleUsers = auth()->user()->role;
+
+    $modulesFiltered = [];
+    foreach ($roleUsers->modules as $key => $module) {
+      
+      $modulesFiltered[$key]['name'] = $module->name;
+
+      foreach ($module->resources as $resource) {
+       if ($resource->roles->contains($roleUsers)) {
+         $modulesFiltered[$key]['resources'][] = $resource;
+       }
+      }
+    }
+   
+    return $view->with('modules', $modulesFiltered);
   }
 }
