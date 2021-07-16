af0408f8a2d2f390f8988c30a08cb50a1f56d59a
Thiel Serra <salathiel.serra@stc.ma.gov.br>
2021-07-16 00:37:06 -0300

    Exibindos modulos -> recursos, como menus para usuários

---
+++ app/Http/Views/MenuViewComposer.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Views;
+
+class MenuViewComposer {
+  public function compose($view)
+  {
+    $menus = auth()->user()
+                    ->role
+                    ->modules;
+                    
+    return $view->with('menus', $menus);
+  }
+}
